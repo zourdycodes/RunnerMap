d0ec9fe22ba19d77d586dd9c3d882a5614ea67ea
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-09 14:37:34 +0700

    add form functionality, need fix later

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+//* Global variable
+let map, mapEvent;
+
 //* Geolocation API
 
 if (navigator.geolocation) {
@@ -21,30 +24,18 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 180,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout Schedule')
-          .openPopup();
+      //! handling click on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus(); // pointing to focus typing input
       });
     },
     function () {
@@ -52,3 +43,33 @@ if (navigator.geolocation) {
     }
   );
 }
+
+//? RENDER map marker
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //* clear input fields
+
+  inputDistance.value = inputDuration.value = inputCadence.value = '';
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 180,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout Schedule')
+    .openPopup();
+});
+
+//* handle input select -> change between running and cycling
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
