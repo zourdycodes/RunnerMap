bc7cdbd0e93404acdc83594c062c6d4976e3c364
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-29 13:26:54 +0700

    fixing: errors when loading the map

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ class App {
 
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
